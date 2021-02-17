9056df9d067ab1f346b4dfcae32dcd8449aa7150
Nguyen Truong <ntruon2@lsu.edu>
2021-02-17 14:32:09 -0600

    bootstrap

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+     'bootstrap4', # new
+
      'users', # new
      'tweets', # new
 ]
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'tweeter_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')), ], # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,6 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tweeter_app.wsgi.application'
+BOOTSTRAP4 = { 'include_jquery': True } # new
 
 
 # Database
